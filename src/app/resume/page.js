27d7e63bc5e27d3d9fcab1159ac879cba7c2706d
Yousung Kim <yousungkim98@gmail.com>
2024-05-31 13:52:22 -0500

    first commit

---
+++ src/app/resume/page.js
@@ -0,0 +1,111 @@
+export default function Resume() {
+  return (
+    <main>
+      <div>
+        <div className="font-bold">About Me</div>
+        <div>Yousung (Ally) Kim</div>
+        <div className="flex flex-row content-center items-center space-x-3">
+          <div className="items-center">
+            <a href="mailto:yousungkim98@gmail.com">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="size-6"
+                height="24"
+                width="24"
+              >
+                <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+                <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+              </svg>
+            </a>
+          </div>
+          <div className="items-center">
+            <a
+              href="https://github.com/yousungk"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 1024 1024"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z"
+                  transform="scale(64)"
+                  fill="#1B1F23"
+                />
+              </svg>
+            </a>
+          </div>
+          <div className="items-center">
+            <a
+              href="https://www.linkedin.com/in/allykim/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="30"
+                height="30"
+                viewBox="0 0 30 30"
+              >
+                <path d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.105,4,24,4z M10.954,22h-2.95 v-9.492h2.95V22z M9.449,11.151c-0.951,0-1.72-0.771-1.72-1.72c0-0.949,0.77-1.719,1.72-1.719c0.948,0,1.719,0.771,1.719,1.719 C11.168,10.38,10.397,11.151,9.449,11.151z M22.004,22h-2.948v-4.616c0-1.101-0.02-2.517-1.533-2.517 c-1.535,0-1.771,1.199-1.771,2.437V22h-2.948v-9.492h2.83v1.297h0.04c0.394-0.746,1.356-1.533,2.791-1.533 c2.987,0,3.539,1.966,3.539,4.522V22z"></path>
+              </svg>
+            </a>
+          </div>
+          <div className="flex flex-row items-center space-x-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="size-6"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <div>201-370-3231</div>
+          </div>
+        </div>
+        <div>Hi, I am...</div>
+      </div>
+      <div>
+        <div className="font-bold">Education</div>
+        <div>University of Pennsylvania</div>
+        <div>Princeton University</div>
+      </div>
+      <div>
+        <div className="font-bold">Experience</div>
+        <div>Technology Intern at AT&T Data Analytics and Insights Team</div>
+        <div>Frontend Software Engineer at Keep.ID</div>
+        <div>Research/Data Analyst at The Brattle Group</div>
+      </div>
+
+      <div>
+        <div className="font-bold">Projects</div>
+        <div>Data Analytics Dashboard (PowerBI Clone)</div>
+        <div>Multimedia Playlist Generator</div>
+        <div>Image Segmentator App</div>
+        <div>Restaurant Booking Data Analyzer</div>
+        <div>AI Video Conference Emotion Detector</div>
+      </div>
+      <div>
+        <div className="font-bold">Technical Skills</div>
+        <div>Python, C++, Java, Javascript/Typescript, HTML, CSS</div>
+        <div>Next.js, React.js, D3.js, Node.js, Express.js, Tailwind</div>
+        <div>AWS, MongoDB, MySQL, PostgreSQL, SQLite</div>
+        <div>Data Analytics Tools: R, Stata</div>
+      </div>
+    </main>
+  );
+}
