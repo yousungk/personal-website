7b798f4d9e9ca1d654de4100fa265117c8b83191
Yousung Kim <yousungkim98@gmail.com>
2024-06-14 15:51:34 -0500

    add usage analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import { Inconsolata } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,7 +14,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inconsolata.className}>{children}</body>
+      <body className={inconsolata.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
